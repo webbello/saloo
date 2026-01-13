e2ed190c6d1bc33feb3c16ebbfc718642f3d4892
Apple <apple@Apples-MacBook-Pro.local>
2026-01-13 18:56:23 +0530

    first commit remove node_modules

--- node_modules/@nuxt/nitro-server/dist/runtime/templates/error-500.d.ts
+++
@@ -1,2 +0,0 @@
-export type DefaultMessages = Record<"appName" | "statusCode" | "statusMessage" | "description" | "refresh", string | boolean | number>;
-export declare const template: (messages: Partial<DefaultMessages>) => string;
--- node_modules/@nuxt/nitro-server/dist/runtime/utils/app-config.d.ts
+++
@@ -1,2 +0,0 @@
-import type { H3Event } from 'h3';
-export declare function useAppConfig(event?: H3Event): any;
