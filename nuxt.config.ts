acf3484674756a294cccdd0e04abbfea23f1e78f
Apple <apple@Apples-MacBook-Pro.local>
2026-01-19 14:05:15 +0530

    feat: set root folder saloo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   
   app: {
+    baseURL: '/saloo/',
     head: {
       title: 'Saloo & Neena Chowdhury - World Record Circumnavigators',
       meta: [
